adb98286552fb3780b1b2debcde2aae387afb67a
Fabricio Faal <fabriciofaal@gmail.com>
2025-04-01 12:07:32 -0300

    Corrigindo erro de importação Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
